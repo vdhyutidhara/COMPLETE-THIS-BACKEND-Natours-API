8d806248931bab0eb9b2bf95386d59b98cf33d78
vdhyutidhara <vinay.dhyutidhara@gmail.com>
2024-01-07 23:51:40 +0530

    Implemented URL filtering features

--- controllers/tourController.js
+++ controllers/tourController.js
@@ -27,8 +27,22 @@ const Tour = require('./../models/tourModel')
 // GET ALL TOURS
 exports.getAllTours = async (req, res) => {
     try {
-        const tours = await Tour.find();
+        // BUILD QUERY
+        // 1) Filtering
+        const queryObj = { ...req.query };
+        const excludedFields = ['page', 'sort', 'limit', 'fields'];
+        excludedFields.forEach(el => delete queryObj[el]);
 
+        // 2) Advance Filtering
+        let queryStr = JSON.stringify(queryObj);
+        queryStr = queryStr.replace(/\b(gte|gt|lte|lt)\b/g, match => `$${match}`);
+
+        const query = Tour.find(JSON.parse(queryStr));
+
+        // EXECUTE QUERY
+        const tours = await query;
+
+        // SEND QUERY
         res.status(200).json({
             status: 'success',
             results: tours.length,

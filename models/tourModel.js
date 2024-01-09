12935f2bccbb40aea9648bacdf119183986b09ae
vdhyutidhara <vinay.dhyutidhara@gmail.com>
2024-01-10 02:31:43 +0530

    middleware mongodb

--- models/tourModel.js
+++ models/tourModel.js
@@ -1,4 +1,5 @@
 const mongoose = require('mongoose');
+const slugify = require('slugify');
 
 const tourSchema = new mongoose.Schema({
     name: {
@@ -7,6 +8,7 @@ const tourSchema = new mongoose.Schema({
         unique: true,
         trim: true
     },
+    slug: String,
     durations: {
         type: Number,
         required: [true, 'A tour must have a duration']
@@ -50,7 +52,11 @@ const tourSchema = new mongoose.Schema({
         type: Date,
         default: Date.now()
     },
-    startDates: [Date]
+    startDates: [Date],
+    secretTour: {
+        type: Boolean,
+        default: false
+    }
 }, {
     toJSON: { virtuals: true },
     toObject: { virtuals: true }
@@ -58,6 +64,45 @@ const tourSchema = new mongoose.Schema({
 
 tourSchema.virtual('durationWeeks').get(function () {
     return this.duration / 7;
+})
+
+//DOCUMENT Middleware: runs before .save() and .create()
+tourSchema.pre('save', function (next) {
+    this.slug = slugify(this.name, { lower: true });
+    next();
+})
+
+// tourSchema.pre('save', function (next) {
+//     console.log('Will save document...');
+//     next();
+// })
+
+// tourSchema.post('save', function (doc, next) {
+//     console.log(doc);
+//     next();
+// })
+
+//Query middleware
+// tourSchema.pre('find', function (next) {
+tourSchema.pre(/^find/, function (next) {
+    this.find({ secretTour: { $ne: true } });
+
+    this.start = Date.now();
+    next();
+})
+
+// tourSchema.post(/^find/, function (docs, next) {
+//     console.log(`Query took ${Date.now() - this.start} milliseconds`);
+//     console.log(docs);
+//     next();
+// })
+
+//Aggregation middleware
+tourSchema.pre('aggregate', function (next) {
+    this.pipeline().unshift({ $match: { secretTour: { $ne: true } } });
+
+    console.log(this.pipeline());
+    next();
 })
 
 const Tour = mongoose.model('Tour', tourSchema);

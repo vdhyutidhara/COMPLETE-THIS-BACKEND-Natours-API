96b24639b3fac49de7d912ec6d9b6de51dd7deda
vdhyutidhara <vinay.dhyutidhara@gmail.com>
2024-01-10 01:56:28 +0530

    Aggregation pipeline

--- routes/tourRoutes.js
+++ routes/tourRoutes.js
@@ -8,6 +8,14 @@ router
     .get(tourController.aliasTopTours, tourController.getAllTours);
 
 router
+    .route('/tour-stats')
+    .get(tourController.getTourStats);
+
+router
+    .route('/monthly-plan/:year')
+    .get(tourController.getMonthlyPlan);
+
+router
     .route('/')
     .get(tourController.getAllTours)
     .post(tourController.createTour);
